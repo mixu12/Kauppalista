fc8bb5935902f8dd3b0ff37039bc5e8d2091388a
mixu12 <kimjouu@gmail.com>
2020-10-23 23:51:43 +0300

    Testi

--- app/src/main/java/com/example/kauppalista/latausikkuna.java
+++ app/src/main/java/com/example/kauppalista/latausikkuna.java
@@ -1,20 +1,27 @@
 package com.example.kauppalista;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class latausikkuna extends Activity {
     public ArrayList<String> arrayList;
     public static String FILE_NAME = null;
+
+    public ListView listaus;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,14 +38,41 @@ public class latausikkuna extends Activity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int) (width*.5),(int)(height*.4));
+        getWindow().setLayout((int) (width*.7),(int)(height*.7));
+
+        setListaus();
     }
 
     public void mainNakyma(View view) {
+
         Intent intent = new Intent(this, MainActivity.class);
-        EditText tiedostonNimi = (EditText) findViewById(R.id.tiedostonNimi);
-        FILE_NAME = tiedostonNimi.getText().toString() +".txt";
+        //EditText tiedostonNimi = (EditText) findViewById(R.id.tiedostonNimi);
+        //FILE_NAME = tiedostonNimi.getText().toString() +".txt";
         intent.putExtra("NIMI", FILE_NAME);
         startActivity(intent);
+    }
+
+    public void setListaus(){
+        File tiedostot = new File(getFilesDir().getPath());
+        File[] tiedostolista = tiedostot.listFiles();
+        final String[] tiedostojenNimet = new String[tiedostolista.length];
+        for (int i = 0; i < tiedostojenNimet.length; i++) {
+            tiedostojenNimet[i] = tiedostolista[i].getName();
+        }
+        listaus = (ListView) findViewById(R.id.listaus);
+        listaus.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        final ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tiedostojenNimet);
+
+        listaus.setAdapter(arrayAdapter);
+
+        listaus.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                EditText tiedostonNimi = (EditText) findViewById(R.id.tiedostonNimi);
+                FILE_NAME = tiedostojenNimet[i].toString();
+                tiedostonNimi.setText(FILE_NAME);
+            }
+        });
+
     }
 }
